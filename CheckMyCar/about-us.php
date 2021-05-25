4ed57e3033b092e48937ba41b2583e8075816eee
Phant0mRay <grsutton@deakin.edu.au>
2021-05-25 22:47:13 +1000

    Updated About-Us page and style.css
    Style.css was updated based on the necessary requirements for the About-Us page.

--- CheckMyCar/about-us.php
+++ CheckMyCar/about-us.php
@@ -40,16 +40,53 @@ include 'main.php';
             <div class="aboutus">
                 <h1>About Us!</h1>
                 <p class="aboutus_pclass">
+                    <br>
                     For project A, our group is to take on the imaginary role of beginning a start-up 
-                    company called ‘CheckYourCar’. The ‘CheckYourCar’ platform intends to connect its 
+                    company called ‘CheckYourCar’. 
+                    <br><br>
+                    The ‘CheckYourCar’ platform intends to connect its 
                     end users with information regarding any know issues and/or problems identified 
-                    with their cars. (i.e. Takata airbag recalls or other known manufacture faults.) 
-                    The team consits of 5 members of whom take upon different roles and positions.<br></p> 
-                    
-                    <h2><br>Ajay Sutton<br>Backend / Security Analyst<br></h2>
-                    <h2><br>Luke Collins<br>Full Stack Development<br><h2>
-                    <h2><br>Mohammed Uddin<br>Database / Securit Analyst<br></h2>
-                    <h2><br>Rhea Sutton <br>Scrum Master / Front End Development<br></h2>
+                    with their cars. (i.e. Takata airbag recalls or other known manufacture faults.)
+                    <hr class="solid">
+                    <div class="row">
+                        <div class="column">
+                            The functionality of this website includes:
+                            <br>
+                            User Login/Registration
+                            <br>
+                            User Forgot Password
+                            <br>
+                            Backend Add Faults/Issues
+                            <br>
+                            Backend Add/Manage users
+                            <br>
+                            Backend Edit Email Templates
+                            <br>
+                            Frontend User Profiles 
+                            <br>
+                            Contact Us Form
+                            <br>
+                            Search Engine (Make and Model)
+                        </div>
+                        <div class="column">
+                            Security:
+                            <br>
+                            2fa
+                            <br>
+                            Brute Force Protection
+                            <br>
+                            CSRF Protection
+                        </div>
+                    </div>
+                    <hr class="solid">
+                    The team consists of 5 members of whom take upon different roles and positions.</p> 
+                    <h2>Ajay Solanki<br>Backend / Security Analyst</h2>
+                    <hr class="invisible">
+                    <h2>Luke Collins<br>Full Stack Development / UI Design<h2>
+                    <hr class="invisible">
+                    <h2>Mohammed Uddin<br>Database / Security Analyst</h2>
+                    <hr class="invisible">
+                    <h2>Rhea Sutton <br>Scrum Master / Front End Development<br></h2>
             </div>
         </main>
 	</body>
